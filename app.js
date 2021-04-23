f2e6bbfc2cc70f3a1a2439ab360aa9a2e679f681
supriyayadav221 <supriyayadav221@gmail.com>
2021-04-24 02:27:31 +0530

    loader

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+
+    document.getElementById('results').style.display ='none';
+
+    document.getElementById('loading').style.display ='block';
+
+    setTimeout(calculateResults,1000);
+
+    e.preventDefault();
+});
 
 
 function calculateResults(e){
@@ -23,15 +32,20 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    document.getElementById('results').style.display ='block';
+
+    document.getElementById('loading').style.display ='none';
   } else {
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
 }
 
 
 function showError(error){
+    document.getElementById('results').style.display ='none';
+
+    document.getElementById('loading').style.display ='none';
   
   const errorDiv = document.createElement('div');
 
